3b606f4da8a14eca611ed2ffbb095cbbc7346ea1
barbiwonderland <baarbarabottazzi@gmail.com>
2025-06-29 20:45:59 -0300

    main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,64 @@
 import { Stack, Link } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
-
+import { ActivityIndicator, FlatList, ScrollView, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Home() {
   const mockData = [
-    { id: '1', name: 'Elemento A' },
-    { id: '2', name: 'Elemento B' },
-    { id: '3', name: 'Elemento C' },
-    { id: '4', name: 'Elemento D' },
-    { id: '5', name: 'Elemento E' },
+    { id: '1', name: 'Elemento A', priority: 'high' },
+    { id: '2', name: 'Elemento B', priority: 'high' },
+    { id: '3', name: 'Elemento C', priority: 'medium' },
+    { id: '4', name: 'Elemento D', priority: 'medium' },
+    { id: '5', name: 'Elemento E', priority: 'medium' },
+    { id: '6', name: 'Elemento F', priority: 'medium' },
+    { id: '7', name: 'Elemento G', priority: 'medium' },
+    { id: '8', name: 'Elemento H', priority: 'medium' },
+    { id: '9', name: 'Elemento I', priority: 'medium' },
+    { id: '10', name: 'Elemento J', priority: 'medium' },
+    { id: '11', name: 'Elemento K', priority: 'medium' },
   ];
 
   type Item = {
     id: string;
     name: string;
+    priority: string;
   };
 
   const [data, setData] = useState<Item[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  // Simular fetch de API
+  // Simular fetch de API descomentar cuand agregues api
   useEffect(() => {
-    fetch('https://example.com/api/items') // Reemplazá por tu API real
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error cargando datos:', error);
-        setLoading(false);
-      });
+    // fetch('https://example.com/api/items') // Reemplazá por tu API real
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     setData(json);
+    //     setLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error cargando datos:', error);
+    //     setLoading(false);
+    //   });
+
+    // const unlockScreenOerientation = async () => {
+    //   await ScreenOrientation.unlockAsync();
+    // };
+    async function changeScreenOrientation() {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    }
+
+    const checkOrientation = async () => {
+      const orientation = await ScreenOrientation.getOrientationAsync();
+      console.log('orientation desde index', orientation);
+    };
+    checkOrientation();
+    // unlockScreenOerientation();
+    changeScreenOrientation();
+    checkOrientation();
   }, []);
 
   return (
@@ -42,24 +66,20 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Barcos v2' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="20-4-2024">
-          <View>
-            <Text className="text-center text-5xl font-bold">Lista</Text>
-
-            {loading ? (
-              <ActivityIndicator size="large" color="#0000ff" />
-            ) : (
-              <FlatList
-                //cuand agreues api cambia aca por data
-                data={mockData}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                  <View className="border border-blue-500 p-4">
-                    <Text className="text-5xl font-bold text-black">{item.name}</Text>
-                  </View>
+          <Text className="text-center font-bold uppercase sm:text-3xl">BUQUE RUISEÑOR</Text>
+          <View className="h-full mx-auto w-11/12  flex-col flex-wrap content-center  py-10">
+            {mockData.map((item) => (
+              <View key={item.id} className=" m-2 flex w-5/12 flex-row  gap-2 items-center  p-4">
+                {item.priority === 'medium' ? (
+                  <AntDesign name="warning" size={24} color="orange" />
+                ) : (
+                  <AntDesign name="closecircleo" size={24} color="red" />
                 )}
-              />
-            )}
+                <Text className="font-bold capitalize text-black">{item.name}</Text>
+              </View>
+            ))}
           </View>
+
           <View className="mt-4 flex-row justify-between gap-4 px-4 ">
             <View className="w-1/2 ">
               <Link href={{ pathname: '/maquinas' }} asChild>
--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -1,6 +1,9 @@
 import { ImageBackground, Text, View } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 import { EditScreenInfo } from './EditScreenInfo';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect } from 'react';
+
 const shipImage = require('../assets/ship.jpg');
 type ScreenContentProps = {
   title: string;
@@ -10,21 +13,35 @@ type ScreenContentProps = {
 
 const date = new Date();
 
+
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+
+useEffect(() => {
+  const unlockScreenOerientation = async () => {
+    await ScreenOrientation.unlockAsync();
+  };
+  const checkOrientation = async () => {
+    const orientation = await ScreenOrientation.getOrientationAsync();
+    console.log('orientation', orientation);
+  };
+  checkOrientation();
+  unlockScreenOerientation();
+}, []);
+
   return (
     <ImageBackground
       source={shipImage}
       style={{ flex: 1, justifyContent: 'center', width: '100%', height: '100%' }}
       resizeMode="cover">
-      <View className="h-full w-full justify-between px-3 py-5">
+      <View className="h-full w-full justify-between px-3 py-5 ">
         <View className="w-full flex-row justify-between gap-5   ">
           <View className="flex flex-row ">
-            <Text className="text-4xl font-bold"> {date.toLocaleDateString()}</Text>
-            <Text className="text-4xl font-bold"> {date.toLocaleTimeString()}</Text>
+            <Text className="sm:text-xl  font-bold"> {date.toLocaleDateString()}</Text>
+            <Text className=" sm:text-xl  font-bold"> {date.toLocaleTimeString()}</Text>
           </View>
           <View className="flex flex-row  gap-2 ">
-            <Feather name="user" size={40} color="black" />
-            <Text className="text-4xl font-bold">Administrator</Text>
+            <Feather name="user" size={20} color="black" />
+            <Text className="sm:text-xl  font-bold">Administrator</Text>
           </View>
         </View>
 
