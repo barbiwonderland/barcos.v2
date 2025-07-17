7cc57e4a18079d073d6222a752e88a00507114dc
barbi <baarbarabottazzi@gmail.com>
2025-07-17 16:47:20 -0300

    agrego api para ver certificados.

--- app/index.tsx
+++ app/index.tsx
@@ -6,21 +6,24 @@ import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import axios from 'axios';
 
 export default function Home() {
-  const mockData = [
-    { id: '1', name: 'Elemento A', priority: 'high' },
-    { id: '2', name: 'Elemento B', priority: 'high' },
-    { id: '3', name: 'Elemento C', priority: 'medium' },
-    { id: '4', name: 'Elemento D', priority: 'medium' },
-    { id: '5', name: 'Elemento E', priority: 'medium' },
-    { id: '6', name: 'Elemento F', priority: 'medium' },
-    { id: '7', name: 'Elemento G', priority: 'medium' },
-    { id: '8', name: 'Elemento H', priority: 'medium' },
-    { id: '9', name: 'Elemento I', priority: 'medium' },
-    { id: '10', name: 'Elemento J', priority: 'medium' },
-    { id: '11', name: 'Elemento K', priority: 'medium' },
-  ];
+  // const mockData = [
+  //   { id: '1', name: 'Elemento A', priority: 'high' },
+  //   { id: '2', name: 'Elemento B', priority: 'high' },
+  //   { id: '3', name: 'Elemento C', priority: 'medium' },
+  //   { id: '4', name: 'Elemento D', priority: 'medium' },
+  //   { id: '5', name: 'Elemento E', priority: 'medium' },
+  //   { id: '6', name: 'Elemento F', priority: 'medium' },
+  //   { id: '7', name: 'Elemento G', priority: 'medium' },
+  //   { id: '8', name: 'Elemento H', priority: 'medium' },
+  //   { id: '9', name: 'Elemento I', priority: 'medium' },
+  //   { id: '10', name: 'Elemento J', priority: 'medium' },
+  //   { id: '11', name: 'Elemento K', priority: 'medium' },
+  // ];
+
+
 
   type Item = {
     id: string;
@@ -34,16 +37,16 @@ export default function Home() {
   // Simular fetch de API descomentar cuand agregues api
   useEffect(() => {
 
-    // axios.get('https://example.com/api/items') // Reemplazá por tu API real
-    //   .then((res) => res.json())
-    //   .then((json) => {
-    //     setData(json);
-    //     setLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error cargando datos:', error);
-    //     setLoading(false);
-    //   });
+axios.get('http://localhost:8082/deck/certificates/expireds') 
+  .then((res) => {
+    console.log(res);      
+    setData(res.data);      
+    setLoading(false);
+  })
+  .catch((error) => {
+    console.error('Error cargando datos:', error);
+    setLoading(false);
+  });
 
   
 
@@ -68,7 +71,7 @@ export default function Home() {
           <Text className="text-center font-bold uppercase sm:text-3xl">BUQUE RUISEÑOR</Text>
           <View className="h-5/6 mx-auto w-11/12  flex-col flex-wrap content-center  py-10">
           {/* aca cambia a data cuando descomentes la peticion con lo de la api */}
-            {mockData.map((item) => (
+            {data.map((item) => (
               <View key={item.id} className=" m-2 flex w-5/12 flex-row  gap-2 items-center bg-white/50 rounded-full  p-4">
                 {item.priority === 'medium' ? (
                   <AntDesign name="warning" size={24} color="orange" />
