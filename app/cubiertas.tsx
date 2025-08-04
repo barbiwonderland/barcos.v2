97fc70d1d4a7a76cb05e8b65b3f559abcd5ddb83
barbiwonderland <baarbarabottazzi@gmail.com>
2025-08-03 21:18:25 -0300

    fixing styles of cubiertas screen

--- app/cubiertas.tsx
+++ app/cubiertas.tsx
@@ -17,21 +17,23 @@ export default function Cubiertas() {
   ];
   return (
       <>
-         <Stack.Screen options={{ title: 'Maquinas' }} />
-         <Container>
-           <View className="h-full w-full bg-[#0A1C34]">
-             <Text className="mt-10 text-center font-bold uppercase text-white sm:text-3xl">
-               SECCIÓN MAQUINAS
-             </Text>
-             <View className="buttons h-2/3  w-2/3 flex-wrap  flex-row gap-5 content-center justify-center mx-auto mt-5">
-               {cubiertasArray && cubiertasArray.length > 0 ? (
-                 cubiertasArray.map((option, index) => <SectionButtons key={index} title={option} />)
-               ) : (
-                 <h1>No hay opciones disponibles</h1>
-               )}
-             </View>
-           </View>
-         </Container>
+  <Stack.Screen options={{ title: 'Cubiertas' }} />
+      <Container>
+        <View className="h-full w-full bg-[#0A1C34] ">
+          <Text className="mt-10 text-center font-bold uppercase text-white sm:text-3xl">
+            SECCIÓN CUBIERTAS
+          </Text>
+          <View className="flex-1 justify-center items-center">
+          <View className="buttons w-full  flex-wrap  flex-row gap-5  justify-center mx-auto mt-5">
+            {cubiertasArray && cubiertasArray.length > 0 ? (
+              cubiertasArray.map((option, index) => <SectionButtons key={index} title={option} />)
+            ) : (
+              <h1>No hay opciones disponibles</h1>
+            )}
+          </View>
+          </View>
+        </View>
+      </Container>
        </>
   );
 }
