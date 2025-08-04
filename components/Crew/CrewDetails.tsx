67ac725f3add6beef188961f135268df861c1081
barbiwonderland <baarbarabottazzi@gmail.com>
2025-08-03 22:51:50 -0300

    base crew components

---
+++ components/Crew/CrewDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function CrewDetails() {
+  return (
+    <View>
+         <Text>Crew Details</Text>
+       </View>
+  )
+}
---
+++ components/Crew/CrewForm.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function CrewForm() {
+  return (
+    <View>
+      <Text>Crew form</Text>
+    </View>
+  );
+}
